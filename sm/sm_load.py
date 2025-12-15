de5df85ba15c411ecfb202be4b31ca38a3704080
Kiliweb42 <kiku-2021@gmx.de>
2025-12-15 15:52:15 +0100

    loanding finally
    finall loading file that takes loadet data from data.db to objects

--- sm/sm_load.py
+++ sm/sm_load.py
@@ -1,6 +1,20 @@
 import sqlite3
+import sm_enemy
 
-con = sqlite3.connect("data.db")
+con = sqlite3.connect("data/data.db")
 cur = con.cursor()
-res = cur.execute("SELECT enemies FROM data")
-res.fetchone()
+enemies_number = 0
+query = f"SELECT COUNT(*) FROM enemies"
+cur.execute(query)
+row_count = cur.fetchone()[0]
+lines =  int(row_count)
+
+
+for i in range(0,lines,1):
+    damage = cur.execute('select damage from enemies where id = 1')
+    hp = cur.execute('select hp from enemies where id = 1')
+    img_path = cur.execute('select img from enemies where id = 1')
+    sm_enemy.sm_enemy(damage,hp,0,0,img_path)
+
+
+
